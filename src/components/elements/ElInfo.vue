3d7c86a9469f2afae040b6e01cfed2a1c92210cc
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:34:28 +0100

    el info

---
+++ src/components/elements/ElInfo.vue
@@ -0,0 +1,64 @@
+<template>
+<div class='infoContainer'
+  v-bind:class="{
+    elInfoHighlight: highlight === true,
+    animation: highlight === true,
+    pointer
+  }"
+  @click="$emit('click')"
+  @mouseover="$emit('mouseover')"
+  @mouseleave="$emit('mouseleave')"
+  >
+  <div v-if="html"
+    v-html="html"/>
+
+  <div>
+    <slot></slot>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'ElInfo',
+  props: ['highlight', 'html', 'pointer']
+}
+
+</script>
+
+<style scoped lang="less">
+  @import '../style/variables';
+
+  .infoContainer {
+    position: relative;
+    width: 100%;
+    height: auto;
+    min-height: 30px;
+    font-size: 12px;
+    line-height: 30px;
+    text-align: left;
+    margin-top: @top-margin-element;
+    color: @grey-darker;
+    background-color: @grey-super-light;
+    padding-left: 10px;
+
+    border: 1px solid @grey-light;
+    border-radius: @radius-small;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  .elInfoHighlight {
+    border: @highlight-border;
+    cursor: pointer;
+
+    &:hover {
+      box-shadow: @shadow;
+    }
+  }
+
+  .pointer {
+    cursor: pointer;
+  }
+</style>
