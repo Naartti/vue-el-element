28cf71e093bc8940bf1eb063d9b0cfaa215afc1f
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:34:58 +0100

    el radio button

---
+++ src/components/elements/ElRadioButton.vue
@@ -0,0 +1,130 @@
+<!--
+
+// HTML
+<el-radio-button
+  v-model="radioModel"
+  :options="radioOptions">
+</el-radio-button>
+
+// JS
+radioModel: 'firstName',
+radioOptions: [{
+  value: 'firstName',
+  label: 'Förnamn'
+}, {
+  value: 'lastName',
+  label: 'Efternamn'
+}, {
+  value: 'emial',
+  label: 'Epost'
+}, {
+  value: 'class',
+  label: 'Klass'
+}, {
+  value: 'teacher',
+  label: 'Undervisande lärare'
+}]
+
+-->
+
+<template>
+<div>
+  <label
+    v-for="(item, index) in options"
+    class="radioContainer"
+    :key="index"
+    :value="value"
+    >
+
+    {{item.label}}
+
+    <input
+      type="radio"
+
+      :id="index"
+      :value="item.value"
+      v-model="checked"
+
+      @click="onChange(item.value)"
+      >
+
+    <span class="checkmark" :value="value"></span>
+  </label>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'ElRadioButton',
+  data () {
+    return {
+      checked: null
+    }
+  },
+  props: ['value', 'options'],
+  mounted () {
+    this.checked = this.value
+  },
+  watch: {
+    value () {
+      this.checked = this.value
+    }
+  },
+  methods: {
+    onChange (value) {
+      this.$emit('input', value)
+      this.$emit('click', value)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @import '../style/variables';
+
+  /* The container */
+  .radioContainer {
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 10px;
+    cursor: pointer;
+    font-size: 12px;
+    line-height: 20px;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  /* Hide the browser's default radio button */
+  .radioContainer input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+  }
+
+  /* Create a custom radio button */
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    border: 1px solid @grey-middle;
+    border-radius: 50%;
+  }
+
+  /* On mouse-over, add a grey background color */
+  .radioContainer:hover input ~ .checkmark {
+    border: 1px solid @green;
+  }
+
+  /* When the radio button is checked, add a blue background */
+  .radioContainer input:checked ~ .checkmark {
+    background-color: @green;
+    border: 1px solid @green;
+  }
+</style>
