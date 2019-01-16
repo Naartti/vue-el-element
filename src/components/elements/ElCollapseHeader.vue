840815f92fe444fd728a917172a2da765162c30b
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:33:58 +0100

    El collapse header

---
+++ src/components/elements/ElCollapseHeader.vue
@@ -0,0 +1,150 @@
+<template>
+<div class='main'>
+  <div class='mainHeader' @click="isExpanded = !isExpanded">
+    <div class="more">
+      <span class="more-circle">
+        <span class="more-icon">
+          <span ref="line1" class="animation more-line more-line-1" v-bind:class="{ transformed1 : isExpanded}"></span>
+          <span ref="line2" class="animation more-line more-line-2" v-bind:class="{ transformed2 : isExpanded}"></span>
+        </span>
+      </span>
+    </div>
+
+    <span>
+        {{title}}
+    </span>
+  </div>
+
+  <transition name="slide-fade">
+    <div v-if="isExpanded" class="collapseContent" key="collapseContent">
+      <slot></slot>
+    </div>
+  </transition>
+
+  <div v-if="isExpanded" class="extensionLine"></div>
+</div>
+</template>
+
+<script>
+
+import CollapseToggle from '@/components/elements/ElCollapseToggle.vue'
+
+export default {
+  name: 'ElCollapseHeader',
+  props: ['title', 'open'],
+  components: { CollapseToggle },
+  data () {
+    return {
+      isExpanded: false
+    }
+  },
+  mounted () {
+    if (this.open === true) this.isExpanded = true
+  }
+}
+
+</script>
+
+<style scoped lang="less">
+  @import '../style/variables';
+
+  .main {
+    position: relative;
+    width: 100%;
+    height: auto;
+    font-size: 18px;
+    text-align: left;
+    margin-top: @top-margin-header;
+    color: @font-color;
+  }
+
+  .mainHeader {
+    padding-left: 30px;
+    width: 100%;
+    cursor: pointer;
+  }
+
+  .collapseContent {
+    margin-left: 30px;
+    width: auto;
+    height: auto;
+  }
+
+  .extensionLine {
+    position: absolute;
+    top: 12px;
+    left: 9px;
+    height: 100%;
+    width: 1px;
+    background-color: @grey-dark;
+  }
+
+  /* collapse icon */
+  .more {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 20px;
+    width: 20px;
+    cursor: pointer;
+  }
+
+  .more-circle {
+    background-color: transparent;
+    top: 2px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+
+  .more-icon {
+    position: absolute;
+    top: 22px;
+    left: 23px;
+    width: 15px;
+    height: 10px;
+    margin: auto;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    transform: rotate(90deg);
+  }
+
+  .more-line {
+    background-color: @grey-darker;
+    height: 2px;
+    width: 12px;
+    border-radius: 1px;
+    position: absolute;
+    left: 0px;
+  }
+
+  .more-line-1 {
+    top: 1px;
+    transform: translateX(2px) rotate(40deg);
+  }
+
+  .more-line-2 {
+    bottom: 1px;
+    transform: translateX(2px) rotate(-40deg);
+  }
+
+  .transformed1 {
+    transform: translateX(3px) rotate(140deg);
+  }
+
+  .transformed2 {
+    transform: translateX(3px) rotate(-140deg);
+  }
+
+  .slide-fade-enter-active {
+    transition: all .5s ease;
+  }
+
+  .slide-fade-enter {
+    transform: translateY(-10px);
+    opacity: 0;
+  }
+</style>
