222fa5f4823b5b86686c0c06c6e9c36b74970df3
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:35:50 +0100

    showcase elements

--- src/components/ShowCase.vue
+++ src/components/ShowCase.vue
@@ -1,20 +1,187 @@
 <template>
 <div>
-  <el-header>Header</el-header>
+  <el-menu>
+    <!-- Text -->
+    <el-menu-item title="Text">
+      <el-header>Header</el-header>
+      <el-caption>Caption</el-caption>
+      <el-info>Information</el-info>
+      <el-message>Message</el-message>
+      <el-message :warning="true">Warning message</el-message>
+    </el-menu-item>
 
-  <el-button>Button</el-button>
-  <el-button :secondary="true">Secondary</el-button>
-  <el-button :danger="true">Danger</el-button>
-  <el-button :secondary="true" :danger="true">Secondary danger</el-button>
+    <!-- Button -->
+    <el-menu-item title="Button">
+      <el-header>Text button</el-header>
+      <el-inline
+        :stretch="true"
+        :wrap="true"
+        :marginTop="true"
+        >
+        <el-button
+          :marginRight="true"
+          :marginTop="true"
+          >
+          Button
+        </el-button>
+        <el-button
+          :marginRight="true"
+          :secondary="true"
+          :marginTop="true"
+          >
+          Secondary
+        </el-button>
+        <el-button
+          :marginRight="true"
+          :danger="true"
+          :marginTop="true"
+          >
+          Danger
+        </el-button>
+        <el-button
+          :marginRight="true"
+          :secondary="true"
+          :danger="true"
+          :marginTop="true"
+          >
+          Secondary danger
+        </el-button>
+        <el-button
+          :disabled="true"
+          :marginTop="true"
+          tooltip="Tooltip"
+          >
+          Disabled
+        </el-button>
+      </el-inline>
 
-  <el-caption>Caption</el-caption>
+      <el-header :marginTop="true">Icon button</el-header>
+      <el-icon-button
+        tooltip="Icon button"
+        icon="?"
+        />
 
-  <el-card>This is a card</el-card>
+      <el-header :marginTop="true">Toggle button</el-header>
+      <el-toggle-button
+        v-model="toggleButton"
+        />
+    </el-menu-item>
+
+    <!-- Input -->
+    <el-menu-item title="Input">
+      <el-input
+        placeholder="Input"
+        />
+      <el-input
+        placeholder="Input with warning"
+        :warning="true"
+        />
+
+      <el-input-button
+        placeholder="Input field"
+        >
+        Input button
+      </el-input-button>
+    </el-menu-item>
+
+    <!-- Select -->
+    <el-menu-item title="Select">
+      <el-checkbox
+        v-model="checkbox.model"
+        :options="checkbox.options"
+        />
+
+      <el-dropdown
+        :options="dropdown.options"
+        v-model="dropdown.model"
+        />
+
+      <el-radio-button
+        :options="dropdown.options"
+        v-model="dropdown.model"
+        />
+
+      <el-select
+        placeholder="Select"
+        v-model="checkbox.model"
+        :options="checkbox.options"
+        />
+    </el-menu-item>
+
+    <!-- Hierarcy -->
+    <el-menu-item title="Hierarcy">
+      <el-card>
+        This is a card
+
+        <el-extend-button :isExpanded="false" />
+      </el-card>
+
+      <el-collapse-header title="Collapse header">Hidden content</el-collapse-header>
+
+      <el-delimiter />
+
+      <el-section>This is a section</el-section>
+
+      <el-button @click="showPopup = true">Show popup</el-button>
+    </el-menu-item>
+  </el-menu>
+
+  <el-popup
+    @close="showPopup = false"
+    v-if="showPopup"
+    >
+    Hello!
+  </el-popup>
 </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      checkbox: {
+        model: [],
+        options: [{
+          value: 'firstName',
+          label: 'Förnamn'
+        }, {
+          value: 'lastName',
+          label: 'Efternamn'
+        }, {
+          value: 'emial',
+          label: 'Epost'
+        }, {
+          value: 'class',
+          label: 'Klass'
+        }, {
+          value: 'teacher',
+          label: 'Undervisande lärare'
+        }]
+      },
+      dropdown: {
+        options: [{
+          value: 'firstName',
+          label: 'Förnamn'
+        }, {
+          value: 'lastName',
+          label: 'Efternamn'
+        }, {
+          value: 'emial',
+          label: 'Epost'
+        }, {
+          value: 'class',
+          label: 'Klass'
+        }, {
+          value: 'teacher',
+          label: 'Undervisande lärare'
+        }],
+        model: 'firstName'
+      },
+      showPopup: false,
+      toggleButton: true
+    }
+  }
+}
 </script>
 <style scoped lang="less">
 </style>
