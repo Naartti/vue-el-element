56b6c0b2859d6f5bf8add778cfce10aa829b4e08
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:35:46 +0100

    global components

--- src/global.js
+++ src/global.js
@@ -1,14 +1,60 @@
 // Global components
+import ElCollapseHeader from '@/components/elements/ElCollapseHeader.vue'
+import ElCollapseToggle from '@/components/elements/ElCollapseToggle.vue'
+import ElStickySection from '@/components/elements/ElStickySection.vue'
+import ElStickyFooter from '@/components/elements/ElStickyFooter.vue'
+import ElToggleButton from '@/components/elements/ElToggleButton.vue'
+import ElExtendButton from '@/components/elements/ElExtendButton.vue'
+import ElInputButton from '@/components/elements/ElInputButton.vue'
+import ElRadioButton from '@/components/elements/ElRadioButton.vue'
+import ElIconButton from '@/components/elements/ElIconButton.vue'
+import ElDelimiter from '@/components/elements/ElDelimiter.vue'
+import ElCheckbox from '@/components/elements/ElCheckbox.vue'
+import ElMenuItem from '@/components/elements/ElMenuItem.vue'
+import ElDropdown from '@/components/elements/ElDropdown.vue'
+import ElMessage from '@/components/elements/ElMessage.vue'
+import ElCaption from '@/components/elements/ElCaption.vue'
+import ElSection from '@/components/elements/ElSection.vue'
+import ElTooltip from '@/components/elements/ElTooltip.vue'
 import ElHeader from '@/components/elements/ElHeader.vue'
 import ElButton from '@/components/elements/ElButton.vue'
-import ElCaption from '@/components/elements/ElCaption.vue'
+import ElInline from '@/components/elements/ElInline.vue'
+import ElSelect from '@/components/elements/ElSelect.vue'
+import ElPopup from '@/components/elements/ElPopup.vue'
+import ElInput from '@/components/elements/ElInput.vue'
+import ElInfo from '@/components/elements/ElInfo.vue'
+import ElText from '@/components/elements/ElText.vue'
 import ElCard from '@/components/elements/ElCard.vue'
+import ElMenu from '@/components/elements/ElMenu.vue'
 
 export default (Vue) => {
-  Vue.component('ElHeader', ElHeader)
-  Vue.component('ElButton', ElButton)
-  Vue.component('ElCaption', ElCaption)
-  Vue.component('ElCard', ElCard)
+  Vue.component('elCollapseHeader', ElCollapseHeader)
+  Vue.component('elCollapseToggle', ElCollapseToggle)
+  Vue.component('elStickySection', ElStickySection)
+  Vue.component('elStickyFooter', ElStickyFooter)
+  Vue.component('elToggleButton', ElToggleButton)
+  Vue.component('ElExtendButton', ElExtendButton)
+  Vue.component('elInputButton', ElInputButton)
+  Vue.component('elRadioButton', ElRadioButton)
+  Vue.component('ElIconButton', ElIconButton)
+  Vue.component('elDelimiter', ElDelimiter)
+  Vue.component('elCheckbox', ElCheckbox)
+  Vue.component('elMenuItem', ElMenuItem)
+  Vue.component('elDropdown', ElDropdown)
+  Vue.component('ElTooltip', ElTooltip)
+  Vue.component('elSection', ElSection)
+  Vue.component('elCaption', ElCaption)
+  Vue.component('ElMessage', ElMessage)
+  Vue.component('elHeader', ElHeader)
+  Vue.component('elButton', ElButton)
+  Vue.component('ElSelect', ElSelect)
+  Vue.component('elInline', ElInline)
+  Vue.component('elInput', ElInput)
+  Vue.component('ElPopup', ElPopup)
+  Vue.component('elInfo', ElInfo)
+  Vue.component('elText', ElText)
+  Vue.component('elCard', ElCard)
+  Vue.component('elMenu', ElMenu)
 
   return Vue
 }
