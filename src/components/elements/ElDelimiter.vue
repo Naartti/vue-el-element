f1094fd5343d9cf8a364e8d2c41f13a4e81f65eb
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:34:08 +0100

    el delimiter

---
+++ src/components/elements/ElDelimiter.vue
@@ -0,0 +1,52 @@
+<template>
+<div class='delimiter'
+  v-bind:class="{
+    delimiterShort : short===true
+  }">
+  <div class='line'
+    v-bind:class="{
+      lineShort : short===true,
+      darkLine : dark===true
+    }">
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'ElDelimiter',
+  props: ['short', 'dark']
+}
+
+</script>
+
+<style scoped lang="less">
+  @import '../style/variables';
+
+  .delimiter {
+    position: relative;
+    width: 100%;
+    height: 70px;
+  }
+
+  .delimiterShort {
+    height: 30px;
+  }
+
+  .line {
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    top: 35px;
+    background-color: @grey-light;
+  }
+
+  .lineShort {
+    top: 10px;
+  }
+
+  .darkLine {
+    background-color: @grey-dark;
+  }
+</style>
