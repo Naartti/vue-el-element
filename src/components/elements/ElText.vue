e286e81f28dfe90d79d56a51e95bb77fe1617ffa
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:35:18 +0100

    el text

---
+++ src/components/elements/ElText.vue
@@ -0,0 +1,29 @@
+<template>
+<div class='textContainer'>
+  {{text}}
+  <slot></slot>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'ElText',
+  props: ['text']
+}
+
+</script>
+
+<style scoped lang="less">
+  @import '../style/variables';
+
+  .textContainer {
+    position: relative;
+    width: 100%;
+    height: auto;
+    font-size: 12px;
+    text-align: left;
+    margin-top: @top-margin-element;
+    color: @grey-darker;
+  }
+</style>
