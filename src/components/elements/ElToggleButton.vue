9e71253f39cf6c47de6dfc3157bf6206bd823d21
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-16 12:35:23 +0100

    el toggle button

---
+++ src/components/elements/ElToggleButton.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="toggleButton"
+  @click="toggle">
+
+  <div class="toggleButton-background animation"
+    ref="background"
+    v-bind:class="{
+        backgroundActivated: checked
+    }">
+
+    <div class="dot animation" ref="dot" v-bind:class="{ dotActivated: checked }"></div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'ElToggleButton',
+  methods: {
+    toggle () {
+      if (this.manual === true) {
+        this.$emit('input', this.checked)
+      } else {
+        this.$emit('input', !this.checked)
+      }
+
+      this.$emit('click')
+    }
+  },
+  props: ['checked', 'manual'],
+  model: {
+    prop: 'checked',
+    event: 'input'
+  }
+}
+
+</script>
+
+<style scoped lang="less">
+  @import '../style/variables';
+
+  .toggleButton {
+    position: relative;
+    display: block;
+    top: 0px;
+    left: 0px;
+    width: auto;
+    height: auto;
+    min-height: 30px;
+    min-width: 60px;
+    margin-top: 15px;
+    padding: none;
+  }
+
+  .toggleButton-background {
+    position: relative;
+    width: 60px;
+    height: 30px;
+    top: 0px;
+    left: 0px;
+    margin: none;
+    margin-left: 0px;
+    background-color: @disable-light;
+    border-radius: 15px;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+
+  .backgroundActivated {
+    background-color: @success-dark;
+  }
+
+  .dot {
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    height: 24px;
+    width: 24px;
+    background-color: #ffffff;
+    border-radius: 50%;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+
+  .dotActivated {
+    left: 33px;
+  }
+</style>
