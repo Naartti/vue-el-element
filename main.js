2c0ef045b2fed33ec8f65558299e2d132c134c31
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-01-17 14:31:06 +0100

    Mass export 🚢

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
-import ElButton from './src/components/elements/ElButton.vue'
-import ElTooltip from './src/components/elements/ElTooltip.vue'
+import ElCollapseHeader from '@/components/elements/ElCollapseHeader.vue'
+import ElCollapseToggle from '@/components/elements/ElCollapseToggle.vue'
+import ElStickySection from '@/components/elements/ElStickySection.vue'
+import ElStickyFooter from '@/components/elements/ElStickyFooter.vue'
+import ElToggleButton from '@/components/elements/ElToggleButton.vue'
+import ElExtendButton from '@/components/elements/ElExtendButton.vue'
+import ElInputButton from '@/components/elements/ElInputButton.vue'
+import ElRadioButton from '@/components/elements/ElRadioButton.vue'
+import ElIconButton from '@/components/elements/ElIconButton.vue'
+import ElDelimiter from '@/components/elements/ElDelimiter.vue'
+import ElCheckbox from '@/components/elements/ElCheckbox.vue'
+import ElMenuItem from '@/components/elements/ElMenuItem.vue'
+import ElDropdown from '@/components/elements/ElDropdown.vue'
+import ElMessage from '@/components/elements/ElMessage.vue'
+import ElCaption from '@/components/elements/ElCaption.vue'
+import ElSection from '@/components/elements/ElSection.vue'
+import ElTooltip from '@/components/elements/ElTooltip.vue'
+import ElHeader from '@/components/elements/ElHeader.vue'
+import ElButton from '@/components/elements/ElButton.vue'
+import ElInline from '@/components/elements/ElInline.vue'
+import ElSelect from '@/components/elements/ElSelect.vue'
+import ElSwitch from '@/components/elements/ElSwitch.vue'
+import ElPopup from '@/components/elements/ElPopup.vue'
+import ElInput from '@/components/elements/ElInput.vue'
+import ElInfo from '@/components/elements/ElInfo.vue'
+import ElText from '@/components/elements/ElText.vue'
+import ElCard from '@/components/elements/ElCard.vue'
+import ElMenu from '@/components/elements/ElMenu.vue'
 
 export {
+  ElCollapseHeader,
+  ElCollapseToggle,
+  ElStickySection,
+  ElStickyFooter,
+  ElToggleButton,
+  ElExtendButton,
+  ElInputButton,
+  ElRadioButton,
+  ElIconButton,
+  ElDelimiter,
+  ElCheckbox,
+  ElMenuItem,
+  ElDropdown,
+  ElMessage,
+  ElCaption,
+  ElSection,
+  ElTooltip,
+  ElHeader,
   ElButton,
-  ElTooltip
+  ElInline,
+  ElSelect,
+  ElSwitch,
+  ElPopup,
+  ElInput,
+  ElInput,
+  ElInfo,
+  ElText,
+  ElCard,
+  ElMenu
 }
