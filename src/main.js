54dce897ea15a5a2a40a48cd3d1bd592858621f1
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-07-18 12:21:41 +0200

    Code highlighting

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueElElements from './wrapper.js'
 
+import hljs from 'highlight.js'
+// import 'highlight.js/styles/a11y-light.css'
+import 'highlight.js/styles/github.css'
+hljs.initHighlightingOnLoad()
+window.hljs = hljs
+window.refreshHljs = () => {
+  hljs.initHighlighting.called = false
+  hljs.initHighlighting()
+}
+
 Vue.config.productionTip = false
 
 Object.entries(VueElElements)
